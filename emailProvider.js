92f80f6ec26a843f4c5c9bb5e4344de6edb062e3
ARAKU-BoyZ <adeeerk@gmail.com>
2024-12-27 14:44:11 +0700

    Latihan

---
+++ emailProvider.js
@@ -0,0 +1,62 @@
+/**
+ * WHAT IS MY EMAIL PROVIDER?
+ * ==========================
+ *
+ * Email merupakan sebuah cara untuk kita berinteraksi antar satu dengan yang lainnya secara elektronik,
+ * Banyak sekali provider yang menyediakan layanan email ini.
+ *
+ * Buatlah sebuah proses yang akan mengeluarkan output provider email yang digunakan oleh user.
+ *
+ * Contoh:
+ *   - Input  : idaz@enigmacamp.com
+ *   - Output : Your email provider is enigmacamp
+ *     (tidak menggunakan .com di belakang)
+ *
+ * RULES:
+ *   - Tidak diperbolehkan menggunakan built-in function:
+ *     .map .filter .reduce .split .join .indexOf .findIndex .substring
+ */
+
+
+
+// input = 'idaz@enigmacamp.com'
+// proses ???
+function emailProvider(input) { // fungsi emailProvider
+    let output = "" // Penampung Kata yang di cari
+    let flag = false // Penanda Looping
+    for (let char of input) { // Tolong looping char dari input
+        if (char === "@") { // jika char bernilai @
+            flag = true // maka ubah flag menjadi true
+            continue // lanjutkan looping
+        }
+        if (char === ".") { // jika char bernilai .(titik)
+            break // maka hentikan looping
+        }
+        if (flag) { // Jika char bernilai true
+            output += char // Maka tambahkan output dengan char
+        }
+    }
+    // return output // kembalikan output
+}
+console.log(emailProvider('idaz@enigmacamp.com')) // output: 'enigmacamp'
+
+// input = 'idaz@xyz.com'
+// proses ???
+function emailProvider(input) { // fungsi emailProvider
+    let output = "" // Penampung Kata yang di cari
+    let flag = false // Penanda Looping
+    for (let char of input) { // Tolong looping char dari input
+        if (char === "@") { // jika char bernilai @
+            flag = true // maka ubah flag menjadi true
+            continue // lanjutkan looping
+        }
+        if (char === ".") { // jika char bernilai .(titik)
+            break // maka hentikan looping
+        }
+        if (flag) { // Jika char bernilai true
+            output += char // Maka tambahkan output dengan char
+        }
+    }
+    return output // kembalikan output
+}
+console.log(emailProvider('idaz@xyz.com')) // output: 'xyz'
